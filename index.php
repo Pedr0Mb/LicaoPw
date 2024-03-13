25479438c754369219a92475223db3d6401e2873
Pedro <pedro.brito50@etec.sp.gov.br>
2024-03-13 17:29:11 -0300

    Primeira mensagem php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeira aula Php</title>
+</head>
+<body>
+<?php
+
+$Nome = "Abner";
+$idade = 16;
+$altura = 1.60;
+$peso = 68;
+var_dump($Nome);
+echo"<br>";
+var_dump($idade);
+echo"<br>";
+var_dump($altura);
+echo"<br>";
+var_dump($peso);
+echo"<br>";
+
+echo "Hello World!";
+echo"<br>";
+echo"Meu Nome é $nome. ";
+?>    
+</body>
+</html>
